e97d56396f788f5ede153953adfd1183e4f8e2ca
Michael Hazani <mhazani@plutovr.com>
2021-06-24 15:26:14 -0700

    remove built in manifest, generate from scratch

--- Editor/BuildXRPKAfterCompile.cs
+++ Editor/BuildXRPKAfterCompile.cs
@@ -5,6 +5,19 @@ using UnityEditor.Callbacks;
 using System.IO;
 using UnityEngine;
 
+
+
+// the XRPK Manifest, necessary to building it from the WebXR build
+[System.Serializable]
+public class XRPKManifest
+{
+    public string name;
+    public string repository;
+    public string description;
+    public string xr_type;
+    public string start_url;
+}
+
 public class BuildXRPKAfterCompile
 {
     [PostProcessBuildAttribute(1)]
@@ -14,40 +27,48 @@ public class BuildXRPKAfterCompile
         string[] s = Application.dataPath.Split('/');
         string projectName = s[s.Length - 2];
 
-        string pathToManifest = Directory.GetCurrentDirectory() + "/Packages/com.pluto.xrpk-exporter/XRPKData/manifest.json";
         string pathToBatchScript = Directory.GetCurrentDirectory() + "/Packages/com.pluto.xrpk-exporter/XRPKData/create-xrpk.bat";
 
         // copy default manifest
         // TODO: ingest JSON, change xrpk name, rewrite manifest
-        File.Copy(pathToManifest, pathToBuiltProject + "/manifest.json", true);
-        UnityEngine.Debug.Log("<color=#FF00FF> XRPK Manifest.json copied to " + pathToBuiltProject + "</color>");
+        XRPKManifest xrpkManifest = new XRPKManifest();
+        xrpkManifest.name = projectName;
+        xrpkManifest.start_url = "index.html";
+        xrpkManifest.description = "A Unity-based XRPK";
+        xrpkManifest.xr_type = "webxr-site@0.0.1";
 
+        string xrpkManifestJSON = JsonUtility.ToJson(xrpkManifest);
+        string JSONdestPath = pathToBuiltProject + "/manifest.json";
+        File.WriteAllText(JSONdestPath, xrpkManifestJSON);
+        UnityEngine.Debug.Log("<color=#FFAAFF> XRPK Manifest.json created in " + pathToBuiltProject + "</color>");
 
-        Process XRPKExecutor = new Process();
+        Process XRPKScriptExecutor = new Process();
         string args = pathToBuiltProject + " " + projectName;
 
         // Unity logger for cmd line stuff
         var sb = new StringBuilder();
         sb.Append("XRPK Creator Log:\n_________________________________");
-        XRPKExecutor.OutputDataReceived += (sender, args) => sb.AppendLine(args.Data);
-        XRPKExecutor.ErrorDataReceived += (sender, args) => sb.AppendLine(args.Data);
-        XRPKExecutor.StartInfo.RedirectStandardInput = true;
-        XRPKExecutor.StartInfo.RedirectStandardOutput = true;
-        XRPKExecutor.StartInfo.RedirectStandardError = true;
-        XRPKExecutor.StartInfo.UseShellExecute = false;
-        XRPKExecutor.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Normal;
-        XRPKExecutor.StartInfo.FileName = pathToBatchScript;
-        XRPKExecutor.StartInfo.Arguments = args;
+        XRPKScriptExecutor.OutputDataReceived += (sender, args) => sb.AppendLine(args.Data);
+        XRPKScriptExecutor.ErrorDataReceived += (sender, args) => sb.AppendLine(args.Data);
+        XRPKScriptExecutor.StartInfo.RedirectStandardInput = true;
+        XRPKScriptExecutor.StartInfo.RedirectStandardOutput = true;
+        XRPKScriptExecutor.StartInfo.RedirectStandardError = true;
+        XRPKScriptExecutor.StartInfo.UseShellExecute = false;
+        XRPKScriptExecutor.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Normal;
+        XRPKScriptExecutor.StartInfo.FileName = pathToBatchScript;
+        XRPKScriptExecutor.StartInfo.Arguments = args;
 
-        UnityEngine.Debug.Log("<color=#FF00FF> Creating XRPK at destination: " + pathToBuiltProject + "</color>");
-        XRPKExecutor.Start();
-        XRPKExecutor.BeginOutputReadLine();
-        XRPKExecutor.BeginErrorReadLine();
-        XRPKExecutor.WaitForExit();
-        File.Delete(pathToBuiltProject + "/manifest.json");
-        UnityEngine.Debug.Log("<color=#FF00FF> Manifest.json deleted from " + pathToBuiltProject + "</color>");
+        UnityEngine.Debug.Log("<color=#FFAAFF> Creating XRPK at destination: " + pathToBuiltProject + "</color>");
+        XRPKScriptExecutor.Start();
+        XRPKScriptExecutor.BeginOutputReadLine();
+        XRPKScriptExecutor.BeginErrorReadLine();
+        XRPKScriptExecutor.WaitForExit();
+
+        //remove manifest after compiling XRPK
+        // File.Delete(JSONdestPath);
+        // UnityEngine.Debug.Log("<color=#FFAAFF> Manifest.json deleted from " + pathToBuiltProject + "</color>");
 
         UnityEngine.Debug.Log(sb);
-        UnityEngine.Debug.Log("<color=#FF00FF> XRPK process done, check above for command line output </color>");
+        UnityEngine.Debug.Log("<color=#FFAAFF> XRPK process done, check above for output </color>");
     }
 }
